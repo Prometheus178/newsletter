d1cbefe9c00c0d405225f05f4bc42b9f0b62395d
siriusonestar <siriusonestar@yandex.ru>
2019-01-27 20:24:16 +0300

    config

---
+++ src/main/java/blog/controller/MainController.java
@@ -0,0 +1,20 @@
+package blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class MainController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView mainView(HttpSession session){
+        WebApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(session.getServletContext());
+         return new ModelAndView("index");
+    }
+}
